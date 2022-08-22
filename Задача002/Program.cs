2052029567340e5861be13e7117eafa838d0bf5a
Elena <bellenaelena@gmail.com>
2022-08-22 19:43:09 +0300

    добавлено решение задачи 002

---
+++ Задача002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter first number: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter last number: ");
+int lasttnumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstnumber > lasttnumber)
+{
+    Console.Write(firstnumber + " > " + lasttnumber);
+}
+else
+{
+    Console.Write(firstnumber + " < " + lasttnumber);
+}
