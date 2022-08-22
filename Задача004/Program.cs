65e662b70f8453e0dda3ff928b112c4429ae6629
Elena <bellenaelena@gmail.com>
2022-08-22 21:36:39 +0300

    добавлено решение задачи 004

---
+++ Задача004/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Enter first number: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter last number: ");
+int lastnumber = Convert.ToInt32(Console.ReadLine());
+
+int max = firstnumber;
+
+
+if (firstnumber < secondnumber)
+{
+     max = secondnumber;
+}
+else
+{
+     max = firstnumber;
+}
+
+if (max < lastnumber)
+{
+    max = lastnumber;
+}
+else
+{
+    max = max;
+}
+
+Console.Write("Maximal number " + max);
