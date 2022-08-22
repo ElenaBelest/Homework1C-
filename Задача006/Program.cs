e1abf3c5c9579e9d4b77b264900b97bbd8e4ac60
Elena <bellenaelena@gmail.com>
2022-08-22 22:14:28 +0300

    добавлено решение задачи 006

---
+++ Задача006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write( number + " -  Even number ");
+}
+else
+{
+    Console.Write( number + " - Odd number ");
+}
