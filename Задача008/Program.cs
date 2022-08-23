f612d1a2e8940b40e217c674516722c970ee2b45
Elena <bellenaelena@gmail.com>
2022-08-23 22:14:36 +0300

    добавлено решение задачи 008

---
+++ Задача008/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Enter number > 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number2 = 1;
+
+if (number <= 0)
+{
+    Console.Write("Bad number");
+}
+else
+{
+    while (number2 <= number)
+    {
+        if (number2 % 2 == 0)
+        {
+            Console.Write(number2 + "  ");
+            number2 ++;
+        }
+        else
+        {
+        number2 ++;
+        }
+    }
+}
